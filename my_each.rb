55618a5a70133b92c40ab559d9198f2c80dbcc11
Erik Weir <erik.weir@key.me>
2020-08-14 12:05:19 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 collection = [1, 2, 3, 4]
 
 
@@ -9,3 +10,12 @@ def my_each(collection)
 end
 collection
 end
+=======
+def my_each(collection) # put argument(s) here
+  # code here
+  collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+  puts i
+end
+end
+>>>>>>> 6e1bf5ff2b02a8f034102a7726feac839f81260c
